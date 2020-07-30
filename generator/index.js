b2779462bfafe5467a7711fa0ef1767d0d0086d3
zhiqiang <simlesos@gmail.com>
2020-07-30 19:16:17 +0800

    feat: 🎸 修改scripts脚本

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,11 @@
 module.exports = (api) => {
-  api.render("./template");
-};
+  api.render('./template')
+
+  api.extendPackage({
+    scripts: {
+      serve: 'cactus serve',
+      build: 'cactus build',
+      lint: 'cactus lint',
+    },
+  })
+}
