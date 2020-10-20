67af183c06eb514da313de00a4dcdc8ab2a049aa
zhiqiang <zhiqiang0x0@outlook.com>
2020-10-20 12:30:56 +0800

    feat: 增加cdn 配置

---
+++ generator/template/cdn.config.js
@@ -0,0 +1,5 @@
+module.exports = function () {
+  // 返回 webpack-cdn-plugin 插件要求的参数
+  // https://www.npmjs.com/package/webpack-cdn-plugin
+  return []
+}
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const WebpackCDNPlugin = require('webpack-cdn-plugin')
+
 module.exports = (api) => {
   api.chainWebpack((config) => {
     // remove prefetch plugin
     // https://github.com/vuejs/preload-webpack-plugin
     config.plugins.delete('prefetch')
+
+    // cdn config
+    // https://www.npmjs.com/package/webpack-cdn-plugin
+    if (fs.existsSync(path.join(process.cwd(), 'cdn.config.js'))) {
+      const CDNConfig = require(path.join(process.cwd(), 'cdn.config.js'))
+      config.plugin('CDNPlugin').use(new WebpackCDNPlugin(CDNConfig))
+    }
   })
 }
